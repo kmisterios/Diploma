33cb7dc878ec100004e63f828595ed7ef6bfb373
kmisterios <kuimov.ms@phystech.edu>
2020-02-13 03:57:46 +0300

    commit4

--- Service/routes.py
+++ Service/routes.py
@@ -6,6 +6,8 @@ from werkzeug.utils import secure_filename
 from Service.forms import LinkForm
 import json
 from jsonschema import validate
+import numpy as np
+from cpd import cpd_count
 
 ALLOWED_EXTENSIONS = {'json'}
 
@@ -31,8 +33,6 @@ def link():
 	path = 'Service/schemas.json'
 	with open(path, 'r') as f:
 		schema = json.loads(f.read())
-	#with open('Service/samples.json', 'r') as f:
-		
 	if request.method == 'POST':
 		# check if the post request has the file part
 		if 'file' not in request.files:
@@ -46,15 +46,40 @@ def link():
 			return redirect(request.url)
 		if file and allowed_file(file.filename):
 			filename = secure_filename(file.filename)
-			#file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			#return redirect(url_for('uploaded_file', filename=filename))
 		samples = json.loads(file.read())
+		num_err = 0
 		for sample in samples:
 			try:
 				validate(instance=sample, schema=schema)
 				flash('Ok \n')
 			except:
+				num_err += 1
 				flash('Error \n')
+		# if there are errors in schemas, it's no reason to continue tests
+		if num_err > 0 :
+			return redirect('/index')
+		# let's try change point detection
+		result_num, type_num = cpd_count(samples, schema, "number")
+		result_len_str = cpd_count(samples, schema, "string")
+		result_len_arr = cpd_count(samples, schema, "array")
+		print(type_num)
+		for result, ttype in np.array([result_num, type_num]).T:
+			if (ttype == np.float64 and len(result) == 1) or (ttype == np.int64 and len(result)- int(result[-1]/5)
+			 + int(result[-1] % 5 in {1,2}) == 1):
+				flash('No anomaly \n')
+			else:
+				flash('Anomaly \n')
+		for result in result_len_str:
+			#print(len(result)- int(result[-1]/5) + int(my_bkps[-1] % 5 in {1,2}), result)
+			if len(result)- int(result[-1]/5) + int(result[-1] % 5 in {1,2}) == 1:
+				flash('No anomaly \n')
+			else:
+				flash('Anomaly \n')
+		for result in result_len_arr:
+			if len(result)- int(result[-1]/5) + int(result[-1] % 5 in {1,2}) == 1:
+				flash('No anomaly \n')
+			else:
+				flash('Anomaly \n')
 		return redirect('/index')
 	return render_template('verify.html', title='Enter your json:', form=form)
 
--- cpd.py
+++ cpd.py
@@ -2,29 +2,33 @@ import ruptures as rpt
 import json
 import numpy as np
 
-def cpd_num(samples, schema):
-	for key in schema["properties"].keys():
-		if schema["properties"][key]["type"] == "number":
-			break
-	numbers = []
-	for sample in samples:
-		numbers.append(sample[key])
-	numbers = np.array(numbers)
-	model="rbf"
-	algo = rpt.Pelt(model=model).fit(prices)
-	result = algo.predict(pen=0.5)
-	return result
 	
-def cpd_len(samples, schema):
+def cpd_count(samples, schema, name):
+	keys = []
+	result = []
+	ttype = []
 	for key in schema["properties"].keys():
-		if schema["properties"][key]["type"] == "string":
-			break
-	numbers = []
-	for sample in samples:
-		numbers.append(sample[key])
-	numbers = np.array(numbers)
-	model="rbf"
-	algo = rpt.Pelt(model=model).fit(prices)
-	result = algo.predict(pen=0.5)
-	return result
-	
+		if schema["properties"][key]["type"] == name:
+			keys.append(key)
+	for key in keys:
+		numbers = []
+		for sample in samples:
+			if name == "number":
+				numbers.append(sample[key])
+			else:	
+				numbers.append(len(sample[key]))
+		numbers = np.array(numbers)
+		ttype.append(type(numbers[0]))
+		if ttype[-1] == np.float64:
+			model="rbf"
+			algo = rpt.Pelt(model=model).fit(numbers)
+			result.append(algo.predict(pen=1))
+		if ttype[-1] == np.int64:
+			model = "l2"
+			algo = rpt.Binseg(model=model).fit(numbers)
+			result.append(algo.predict(n_bkps=10))
+	if name == "number":
+		return result, ttype
+	else:
+		return result
+
